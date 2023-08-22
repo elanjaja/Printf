942304cabf39d4af4d1eb7eeaf9d2b84520f5c0a
Patience Joshua <128899827+PatJosh-Codes@users.noreply.github.com>
2023-08-22 12:40:49 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -2,19 +2,19 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - Prints a string.
  * @c: char types.
- * @buffer: Buffer array to handle print
+ * @buffer: Buffer array to handle print.
  * @flags:  Calculates active flags.
  * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
+ * @precision: precision specifier.
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
 int flags, int width, int precision, int size)
-{/* char is stored at left and paddind at buffer's right */
+{/* char is stored at left and padding at buffer's right */
 int i = 0;
 char padd = ' ';
 
