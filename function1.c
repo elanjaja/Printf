34ba92f858e36c04808e8227d49a6a1bbe201159
Patience Joshua <128899827+PatJosh-Codes@users.noreply.github.com>
2023-08-22 12:32:48 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -2,11 +2,11 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * print_unsigned - Prints an unsigned number.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
